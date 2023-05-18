a05cd7af5bd43d4cf65aea8cf2cf1b142e0c64b5
Kevin Thomas <kevinthomasny@gmail.com>
2023-05-17 23:15:49 -0400

    Updated portfolio site link

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -145,7 +145,7 @@ const Sidebar = () => {
                 </li>
                 <li>
                   <a
-                    href="https://kevin-thomas-portfolio.vercel.app/"
+                    href="https://kevinthomas.vercel.app/"
                     target="_blank"
                     className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                   >
